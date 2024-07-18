8f8efb77091b185cc9df25b5757d01f588448064
Luis Carlos - [front] <luisestrela.bol951@gmail.com>
2024-07-18 03:51:38 -0300

    fix: remove cursor on mobile

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -6,19 +6,17 @@ import { useIsMobile } from '@/hooks';
 export function Cursor() {
   const isMobile = useIsMobile();
 
-  return (
-    !isMobile && (
-      <AnimatedCursor
-        innerStyle={{
-          background: '#7E8285',
-        }}
-        outerSize={40}
-        outerScale={1.2}
-        outerStyle={{
-          background: 'none',
-          border: '2px solid #7E8285',
-        }}
-      />
-    )
-  );
+  return !isMobile ? (
+    <AnimatedCursor
+      innerStyle={{
+        background: '#7E8285',
+      }}
+      outerSize={40}
+      outerScale={1.2}
+      outerStyle={{
+        background: 'none',
+        border: '2px solid #7E8285',
+      }}
+    />
+  ) : null;
 }
